a62bb3ed3def0cdd6c48418e20e28f868d25fb11
lukaivanovic <lluka.ivanovic@gmail.com>
2024-11-26 14:10:12 +0100

    add repeat

--- ww-config.js
+++ ww-config.js
@@ -88,10 +88,31 @@ export default {
             defaultValue: 'vertical',
             bindable: true,
         },
-
         contentLayout: {
-            hidden: true,
+            label: {
+                en: 'Items',
+                fr: 'Items',
+            },
+            type: 'Repeat',
+            options: {
+                text: { en: 'Elements to repeat' },
+            },
+            hidden: (content, sidePanelContent, boundProps, wwProps) => !!(wwProps && wwProps.isFixed),
+            bindable: 'repeatable',
             defaultValue: [],
+            /* wwEditor:start */
+            bindingValidation: {
+                validations: [
+                    {
+                        type: 'array',
+                    },
+                    {
+                        type: 'object',
+                    },
+                ],
+                tooltip:
+                    'A collection or an array of data: \n\n`myCollection` or `[{}, {}, ...] || ["string1", "string2", ...] || [1, 2, ...]`',
+            },
         },
     },
 };
