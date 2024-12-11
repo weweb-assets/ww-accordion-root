41af51a566fc4bc064da0a3ebdfb823de3d0a7bc
luka-weweb <luka@weweb.io>
2024-12-11 18:35:32 +0100

    log

--- src/wwElement.vue
+++ src/wwElement.vue
@@ -38,6 +38,7 @@ export default {
         watch(
             () => type.value,
             newType => {
+                console.log('newType', newType);
                 setComponentValue(newType === 'single' ? '' : []);
             }
         );
